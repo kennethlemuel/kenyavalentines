f53dd3aea56cabf57f8e609c1a9142ef0abd7dbd
kenneth <kenneth004@e.ntu.edu.sg>
2024-02-04 04:23:21 +0800

    initial

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     button.addEventListener('click', ()=> alert('You clicked me'));
     button.addEventListener('mouseover', () => {
-        console.log('You hovered me');
         button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
         button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
     })
