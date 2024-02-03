a89868d79b8c6d6b8a132c137c92862e7a3c392d
kenneth <kenneth004@e.ntu.edu.sg>
2024-02-04 03:43:43 +0800

    intial

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttonHeight = 50;
+const buttonWidth = 150;
+
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button');
+
+    button.addEventListener('click', ()=> alert('You clicked me'));
+
+});
