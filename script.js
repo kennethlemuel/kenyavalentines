c47d9e692dd9614da3520b7f2986db4722c79e1a
kenneth <kenneth004@e.ntu.edu.sg>
2024-02-04 04:18:57 +0800

    initial

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const buttonHeight = 50;
 const buttonWidth = 150;
 
+const maxWidth = window.innerWidth - buttonWidth;
+const maxHeight = window.innerHeight - buttonHeight;
+
 window.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('no_button');
 
     button.addEventListener('click', ()=> alert('You clicked me'));
     button.addEventListener('mouseover', () => {
         console.log('You hovered me');
+        button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+        button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
     })
+
+
 
 });
