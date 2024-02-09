085c3ad46049fb53170526083625bd3a2a4543ae
kenneth <kenneth004@e.ntu.edu.sg>
2024-02-09 22:33:08 +0800

    hopefully works!!

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ window.addEventListener('DOMContentLoaded', () => {
     const no_button = document.getElementById('no_button');
     const yes_button = document.getElementById('yes_button'); 
 
-    no_button.addEventListener('click', ()=> alert('You clicked me'));
     no_button.addEventListener('mouseover', () => {
-        no_button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
-        no_button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
+        const newLeft = Math.floor(Math.random() * (maxWidth + 1));
+        const newTop = Math.floor(Math.random() * (maxHeight + 1));
+        no_button.style.left = Math.min(newLeft, maxWidth) + 'px';
+        no_button.style.top = Math.min(newTop, maxHeight) + 'px';
     })
 
     yes_button.addEventListener('click', ()=>{
