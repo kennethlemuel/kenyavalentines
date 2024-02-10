7c2741eb048d96ae9e73cce75d7eca63911d2e2f
kenneth <kenneth004@e.ntu.edu.sg>
2024-02-11 01:55:33 +0800

    new page

--- script.js
+++ script.js
@@ -8,16 +8,18 @@ window.addEventListener('DOMContentLoaded', () => {
     const no_button = document.getElementById('no_button');
     const yes_button = document.getElementById('yes_button'); 
 
+    no_button.addEventListener('click', ()=> alert('You clicked me'));
     no_button.addEventListener('mouseover', () => {
-        const newLeft = Math.floor(Math.random() * (maxWidth + 1));
-        const newTop = Math.floor(Math.random() * (maxHeight + 1));
-        no_button.style.left = Math.min(newLeft, maxWidth) + 'px';
-        no_button.style.top = Math.min(newTop, maxHeight) + 'px';
+        no_button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+        no_button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
     })
 
     yes_button.addEventListener('click', ()=>{
         window.open('yes.html', '_self');
     })
 
-
 });
+
+
+
+
